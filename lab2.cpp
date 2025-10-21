b09d6d92b3651f159cee8e53e3989e319883310b
zuzia <zuzia19735@gmail.com>
2025-10-21 15:11:54 +0200

    Add files via upload

---
+++ lab2.cpp
@@ -0,0 +1,69 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    // zad 1
+    /* int N;
+    cout << "Podaj ilosc kolejnych liczb calkowitych: " << endl;
+    cin >> N;
+    for (int i = 1; i <= N; i++)
+        { 
+        cout << i << endl;
+        } */
+    // zad 2
+    /* int N;
+    cout << "Podaj ilosc kolejnych liczb calkowitych: " << endl;
+    cin >> N;
+    while ( N != 1)
+        {
+            N = (N - 1);
+            cout << N << endl;
+        } */
+    // zad 3
+    /*
+    double L, max, min, ave, il=0, suma=0;
+    char odp;
+    do{
+        cout << "Podaj liczbe: " << endl;
+        cin >> L;
+        cout << "Czy to koniec wprowadzania wartosci?(T/N)";
+        cin >> odp;
+        suma = suma + L;
+        il++;
+        ave = suma / il;
+        if (il == 1) {
+            min = L;
+            max = L;
+        }
+
+        if (min > L)
+        {
+            min = L;
+        }
+        if (max < L)
+        {
+            max = L;
+        }
+        
+      
+    } while (odp == 'N');
+    cout << "Minimum wynosi: " << min << endl;
+    cout << "Maximum wynosi: " << max << endl;
+    cout << "Srednia wynosi: " << ave << endl; */
+    int a, b, c, ave, k;
+    char odp;
+    cout << "Wymysl liczbe i podaj zakres, w ktorym sie znajduje: " << endl;
+    cin >> a >> b;
+    while (a!=b)
+    { 
+        ave=(a+b)/2
+            cout << ""
+
+
+        
+
+
+    
+            
+}
