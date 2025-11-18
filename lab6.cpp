3cf6dfc91782c9071253f8474e042f32d0cab63c
zuzia <zuzia19735@gmail.com>
2025-11-18 15:10:38 +0100

    Add files via upload

---
+++ lab6.cpp
@@ -0,0 +1,67 @@
+#include <iostream>
+#include <cmath>
+using namespace std;
+// zad 1
+/* double szescian(float a) {
+    return a * a * a;
+}
+int main()
+{
+    float a, wynik;
+    cin >> a;
+    wynik = szescian(a);
+    cout << "Szescian liczby wynosi: " << wynik << endl;
+
+} */
+// zad 2
+/* double potega(int n, double c) {
+    double wynik=1;
+    if (n == 0)
+    {
+        return 1;
+    }
+    else
+    {
+        for (int i=1; i <= n; i++)
+        {
+   
+           
+           
+            wynik = wynik * c;
+           
+        }
+        return wynik;
+    }
+}
+int main()
+{
+    double c, wynik;
+    int n;
+    cin >> c;
+    cin >> n;
+    wynik = potega(n, c);
+    cout << "Potega liczby wynosi: " << wynik << endl;
+
+} */
+// zad 3
+/*
+double odleglosc_euklidesowa(double x1, double x2, double y1, double y2)
+{
+    double roznicax = (x2 - x1) * (x2 - x1);
+    double roznicay = (y2 - y1) * (y2 - y1);
+    return sqrt(roznicax + roznicay);
+}
+int main()
+{
+    double x1, x2, y1, y2;
+    cout << "Wprowadz x1, x2, y1, y2: " << endl;
+    cin >> x1 >> x2 >> y1 >> y2;
+    double wynik = odleglosc_euklidesowa(x1,x2,y1,y2);
+    cout << "Wynik wynosi: " << wynik << endl;
+
+} */
+// zad 4
+string ilosc_a(string a)
+{
+
+}
