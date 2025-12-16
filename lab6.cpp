7e38e61ecd14a6c1a47e116d867fd7d82407f990
zuzia <zuzia19735@gmail.com>
2025-12-16 15:11:01 +0100

    Add files via upload

--- lab6.cpp
+++ lab6.cpp
@@ -1,40 +1,71 @@
-#include <vector>
 #include <iostream>
+#include <cmath>
 using namespace std;
-//int main()
-//{
-	//zad 1
-	/*
-	
-	vector<int> wektor;
-	for (int i = 0; i < 10; i++)
-	{
-		wektor.push_back(rand() % 101);
-	}
-	for (int i = 0; i < wektor.size(); i++)
-	{
-		cout << wektor[i] << endl;
-	} */
-	// zad 2
-int tworzeniewektorow(int a)
+struct punkt
 {
-	vector<int> wektor;
-	for (int i = 0; i < 10; i++)
-	{
-		wektor.push_back(rand() % 101);
-	}
-	for (int i = 0; i < wektor.size(); i++)
-	{
-		return wektor[i];
-	}
+    double x, y;
+};
+struct prostokat
+{
+    punkt p1, p2;
+};
+struct kolo
+{
+    punkt s;
+    double promien;
+};
+double odleglosc(punkt p1, punkt p2)
+{
+    double d = sqrt(pow((p2.x - p1.x), 2) + pow((p2.y - p1.y), 2));
+    return d;
+}
+double pole_prostokata(prostokat p)
+{
+    double a = abs(p.p2.x - p.p1.x);
+    double b = abs(p.p2.y - p.p1.y);
+    double pole = a * b;
+    return pole;
+}
+double pole_kola(kolo k)
+{
+    double wynik = (k.promien * k.promien) * 3.14159265359;
+    return wynik ;
 }
 int main()
-	{
-	int a;
-	int tablica = tworzeniewektorow(a);
-	cout << tworzeniewektorow(a) << endl;
-	}
-
-
-
-//}
+{
+   /* punkt p1, p2;
+    p1.x = 0;
+    p1.y = 0;
+    p2.x = 1;
+    p2.y = 1;
+    cout << "Odleglosc miedzy punktami wynosi " << odleglosc(p1, p2) << endl; */
+    /*
+    punkt p1, p2;
+    p1.x = 0;
+    p1.y = 0;
+    p2.x = 2;
+    p2.y = 1;
+    cout << "Pole prostokata wynosi: " << pole_prostokata(p1, p2) << endl; */
+    /* prostokat po1;
+    po1.p1.x = 0;
+    po1.p1.y = 0;
+    po1.p2.x = 1;
+    po1.p2.y = 1;
+    prostokat po2;
+    po2.p1.x = 0;
+    po2.p1.y = 0;
+    po2.p2.x = 2;
+    po2.p2.y = 1;
+    if (pole_prostokata(po1) > pole_prostokata(po2))
+    {
+        cout << "Wieksze pole prostokata wynosi: " << pole_prostokata(po1) << endl;
+    }
+    else
+    {
+        cout << "Wieksze pole prostokata wynosi: " << pole_prostokata(po2) << endl;
+    } */
+    /* kolo k;
+    k.promien = 4;
+    cout << "Pole kola wynosi: " << pole_kola(k) << endl; */
+   
+}
